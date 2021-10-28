32fa3583577460130421e95341b812b85df7f999
Yuliieth1104 <kelly.yuliieth@gmail.com>
2021-10-28 11:31:06 -0500

    2

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <h4>Yuliieth1104</h4>
+    </div>
+    <div>
+      <a href="https://github.com/Yuliieth1104">GitHub</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
